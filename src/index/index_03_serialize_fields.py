d766bbd00f665debe0d25138f1a3bea7daa76c4f
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2019-09-18 22:01:12 -0300

    Deployed 1364f73 with MkDocs version: 1.0.4

--- src/index/index_03_serialize_fields.py
+++ src/index/index_03_serialize_fields.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, TypedDict
 
-from dataclassesjson import asdataclass, asjson, dataclassjson
+from typingjson import (
+    as_typed_dict,
+    asdataclass,
+    dataclass_asjson,
+    typed_dict_asjson,
+    typingjson,
+)
 
 
 @dataclass
@@ -9,7 +15,7 @@ class Music:
     name: str
 
 
-@dataclassjson(serialize_fields=('name', 'age'))
+@typingjson(serialize_fields=('name', 'age'))
 @dataclass
 class Person:
     name: str
@@ -20,5 +26,30 @@ class Person:
 jsondict = dict(name='John', age=40, musics=[dict(name='Imagine')])
 person = asdataclass(jsondict, Person)
 
+print('dataclass:')
 print(person)
-print(asjson(person))
+print(dataclass_asjson(person))
+print()
+
+
+# TypedDict
+
+
+@typingjson
+class Music(TypedDict):
+    name: str
+
+
+@typingjson(serialize_fields=('age'))
+class Person(TypedDict):
+    name: str
+    age: int
+    musics: List[Music]
+
+
+jsondict = dict(name=b'John', age='40', musics=[dict(name='Imagine')])
+person = as_typed_dict(jsondict, Person)
+
+print('TypedDict:')
+print(person)
+print(typed_dict_asjson(person, Person))
